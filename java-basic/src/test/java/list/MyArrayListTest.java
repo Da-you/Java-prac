a9af2035d068d9ae62817e7b2050e213d6c1c93e
JoHyunwoo <jhw3874@gmail.com>
2024-06-10 00:02:27 +0900

    ArrayList

---
+++ java-basic/src/test/java/list/MyArrayListTest.java
@@ -0,0 +1,48 @@
+package list;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyArrayListTest {
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void insert() {
+  }
+
+  @Test
+  void clear() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void deleteByIndex() {
+  }
+
+  @Test
+  void get() {
+  }
+
+  @Test
+  void indexOf() {
+  }
+
+  @Test
+  void isEmpty() {
+  }
+
+  @Test
+  void contains() {
+  }
+
+  @Test
+  void size() {
+  }
+}
